7f516da3c5c5a79bceede12731c8edb4c9ec2010
PRoman93 <roman_prokopets@mail.ua>
2020-01-27 13:41:28 +0200

    63-th lesson is done

---
+++ src/api/Api.js
@@ -0,0 +1,23 @@
+import * as axios from "axios";
+
+const instanse = axios.create({
+    withCredentials:true,
+    baseURL:'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '9a75d041-72ac-4332-ad7a-749ea855f8b0'
+    }
+})
+export const usersAPI = {
+    getUsers (currentPage = 1, pageSize = 10)  {
+        return instanse.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
+}
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+return instanse.get(`follow?page=${currentPage}&count=${pageSize}`)
+    .then(response => {
+        return response.data
+    })
+}
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -12,32 +12,28 @@ import {
 import * as axios from "axios";
 import loader from './Spinner-1s-200px.svg'
 import Preloader from "../../common/preloader/Preloader";
+import {usersAPI} from "../../api/Api";
 
 class UsersContainer extends React.Component {
 
 
     componentDidMount() {
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
-            withCredentials:true
-        })
-            .then(response => {
+     usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
                 this.props.toggleIsFetching(false)
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
+                this.props.setUsers(data.items)
+                this.props.setTotalUsersCount(data.totalCount)
             })
     }
 
     onPageChanged = (pageNumber) => {
         this.props.toggleIsFetching(true)
         this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,{
-            withCredentials:true
-        })
-            .then(response => {
+      usersAPI.getUsers(pageNumber, this.props.pageSize)
+            .then(data => {
                 this.props.toggleIsFetching(false)
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
+                this.props.setUsers(data.items)
+                // this.props.setTotalUsersCount(response.data.totalCount)
 
             })
     }
