c2925428d2eb0f7ddac5b985f7f71433873344c9
PRoman93 <roman_prokopets@mail.ua>
2020-02-15 15:38:11 +0200

    94-th lesson is done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,18 +4,21 @@ import Navbar from "./Navbar/Navbar"
 import News from "./News/News";
 import Music from "./Music/Music"
 import Settings from "./Settings/Settings"
-import {Route, withRouter} from "react-router-dom";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import DialogsContainer from "./Dialogs/DialogsContainer";
 import UsersContainer from "../Users/UsersContainer";
 import ProfileContainer from "./Profile/ProfileContainer";
 import Login from "../Login/Login";
 import {compose} from "redux";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {initializeApp} from "../../state/app-reducer";
+import {withSuspense} from "../../hoc/withSuspense";
+import store from "../../state/redux-store";
 import Preloader from "../../common/preloader/Preloader";
 
-
 class Main extends Component {
+
+
 
 
     componentDidMount() {
@@ -23,6 +26,10 @@ class Main extends Component {
     }
 
     render() {
+
+        const DialogsContainer = React.lazy(() => import('./Dialogs/DialogsContainer'));
+        const ProfileContainer = React.lazy(() => import('./Profile/ProfileContainer'));
+
         if (!this.props.initialized) {
             return <Preloader/>
         }
@@ -31,8 +38,8 @@ class Main extends Component {
                 <div className={s.container}>
                     <Navbar/>
                     <div className={s.wrapperContent}>
-                        <Route path='/dialogs' render={() => <DialogsContainer store={this.props.store}/>}/>
-                        <Route path='/profile/:userId?' render={() => <ProfileContainer store={this.props.store}/>}/>
+                        <Route path='/dialogs'  render={withSuspense(DialogsContainer)}/>
+                        <Route path='/profile/:userId?'  render={withSuspense(ProfileContainer)}/>
                         <Route path='/news' component={News}/>
                         <Route path='/music' component={Music}/>
                         <Route path='/settings' component={Settings}/>
@@ -46,7 +53,16 @@ class Main extends Component {
 const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
-export default
-compose(withRouter, connect(mapStateToProps, {initializeApp}))(Main)
+let AppContainer = compose(
+    withRouter,
+    connect(mapStateToProps, {initializeApp}))(Main);
 
-//connect(mapStateToProps, {initializeApp})(Main);
+const SamuraiJSApp = (props) => {
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer />
+        </Provider>
+    </BrowserRouter>
+}
+
+export default SamuraiJSApp;
