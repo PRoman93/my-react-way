ce50cff1ca9dba8fae400e12e0cbce0820e50dcb
PRoman93 <roman_prokopets@mail.ua>
2020-03-09 13:05:45 +0200

    any refactoring

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -30,7 +30,6 @@ const Login = (props) => {
     const onSubmit = (formData) => {
         props.login(formData.email, formData.password, formData.rememberMe)
     }
-
     if (props.isAuth){
         return <Redirect to={'/profile'}/>
     }
